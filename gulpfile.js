72446d8c80dc531430d5288b9b60cd54aa74b28c
gauravdesale <desalegaurav60@gmail.com>
2017-09-24 15:12:02 -0500

    right now have just add the uglify and if functionality to reduce the size of the html script tags and minify the main one and later will add  sass  processing basic functionality

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,25 @@ var
     build: 'build/'
   }
 ;
+
+
+gulp.task('hello', function(){
+	console.log('Hello zell');
+});
+
+gulp.task('task-name', function () {
+  return gulp.src('test.php') // Get source files with gulp.src
+    .pipe(gulp-php-minify()) // Sends it through a gulp plugin
+    .pipe(gulp.dest('firstAutomation/php')
+})
+
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
